faad9b514906ab0fcc9b5c4749be574e6e6f2df1
monostylar <xtur16+monostylar@gmail.com>
2024-10-21 04:18:47 +0700

    Update bb.py

--- bb.py
+++ bb.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains  # Untuk action mouse
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import random
 import string
 import pyperclip
+
 
 # Konfigurasi driver
 driver = webdriver.Chrome()  # Menggunakan ChromeDriver yang ada di sistem
